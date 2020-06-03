5fa52e394bb6642ff3555195141c792e03c1ee73
Ben Schmitt <benschmitt@MacBook-Pro.local>
2020-06-03 18:05:36 -0400

    'master'

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,21 @@
-# pcost.py
-#
-# Exercise 1.27
+import csv
+import sys
+
+def portfolio_cost(filename):
+    f = open(filename)
+    total_cost = 0
+    rows = csv.reader(f)
+    headers = next(rows)
+
+    for row in rows:
+        total_cost += int(row[1]) * float(row[2])
+
+    return total_cost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print("Total cost of portfolio: ", cost)
